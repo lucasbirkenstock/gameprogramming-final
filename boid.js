4422c4b8acfa5dc9c9fc4257195e0c045894eb42
Lucas Birkenstock <lucasbirkenstock@protonmail.com>
2024-11-30 17:12:28 -0800

    Dynamically change animations based on angle. Angle calculation needs to be changed

--- boid.js
+++ boid.js
@@ -1,37 +1,142 @@
 class Boid {
-    constructor() {
+  constructor() {
       this.position = createVector(random(width), random(height));
       this.velocity = p5.Vector.random2D();
       this.velocity.setMag(random(2, 4));
       this.acceleration = createVector();
       this.maxForce = 0.2;
       this.maxSpeed = 5;
+      this.angle = this.velocity.heading();
 
-      // Animation frames
-      this.frames = [];
+      // Animation frames for cardinal and intercardinal directions
+      this.animations = {
+          north: [],
+          northeast: [],
+          east: [],
+          southeast: [],
+          south: [],
+          southwest: [],
+          west: [],
+          northwest: []
+      };
+      this.currentAnimation = this.animations.south; // Default animation
       this.currentFrame = 0;
       this.frameDelay = 5;
       this.frameCounter = 0;
       this.spriteWidth = 50;
       this.spriteHeight = 50;
-    }
+  }
 
-    loadFrames(animationPath, frameCount)
-    {
-        for (let i = 0; i < frameCount; i++)
-        {
-            this.frames.push(loadImage(`${animationPath}/${i}.png`))
-        }
-    }
-  
-    edges() 
+  loadFrames(direction, animationPath, frameCount) {
+      // Loads frames for a specific direction
+      if (!this.animations[direction]) {
+          console.error(`Invalid direction: ${direction}`);
+          return;
+      }
+      for (let i = 0; i < frameCount; i++) {
+          this.animations[direction].push(loadImage(`${animationPath}/${i}.png`));
+      }
+  }
+
+  determineDirection() {
+      // Determine the direction based on the angle
+      const angle = this.angle;
+
+      if (angle > -PI / 8 && angle <= PI / 8) {
+          return "east";
+      } else if (angle > PI / 8 && angle <= (3 * PI) / 8) {
+          return "southeast";
+      } else if (angle > (3 * PI) / 8 && angle <= (5 * PI) / 8) {
+          return "south";
+      } else if (angle > (5 * PI) / 8 && angle <= (7 * PI) / 8) {
+          return "southwest";
+      } else if (angle > (7 * PI) / 8 || angle <= -(7 * PI) / 8) {
+          return "west";
+      } else if (angle > -(7 * PI) / 8 && angle <= -(5 * PI) / 8) {
+          return "northwest";
+      } else if (angle > -(5 * PI) / 8 && angle <= -(3 * PI) / 8) {
+          return "north";
+      } else if (angle > -(3 * PI) / 8 && angle <= -PI / 8) {
+          return "northeast";
+      }
+  }
+
+  update() {
+      this.position.add(this.velocity);
+      this.velocity.add(this.acceleration);
+      this.velocity.limit(this.maxSpeed);
+      this.acceleration.mult(0);
+
+      // Update the angle based on the velocity
+      this.angle = this.velocity.heading();
+
+      // Determine the current direction and switch animation frames
+      const direction = this.determineDirection();
+      this.currentAnimation = this.animations[direction];
+
+      // Update frame for animation
+      this.frameCounter++;
+      if (this.frameCounter >= this.frameDelay) {
+          this.currentFrame = (this.currentFrame + 1) % this.currentAnimation.length;
+          this.frameCounter = 0;
+      }
+  }
+
+  show() {
+      push();
+      translate(this.position.x, this.position.y);
+      rotate(this.angle);
+
+      if (this.currentAnimation.length > 0) {
+          image(
+              this.currentAnimation[this.currentFrame],
+              -this.spriteWidth / 2,
+              -this.spriteHeight / 2,
+              this.spriteWidth,
+              this.spriteHeight
+          );
+      }
+      pop();
+  }
+
+  edges() 
     {
         if (this.position.x > width) this.position.x = 0;
         else if (this.position.x < 0) this.position.x = width;
         if (this.position.y > height) this.position.y = 0;
         else if (this.position.y < 0) this.position.y = height;
     }
-  
+
+    flock(boids) 
+    {
+        let alignment = this.align(boids);
+        let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+    
+        alignment.mult(alignSlider.value());
+        cohesion.mult(cohesionSlider.value());
+        separation.mult(separationSlider.value());
+    
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
+        this.acceleration.add(separation);
+    }
+
+    flock(boids) 
+    {
+        let alignment = this.align(boids);
+        let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+    
+        alignment.mult(alignSlider.value());
+        cohesion.mult(cohesionSlider.value());
+        separation.mult(separationSlider.value());
+    
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
+        this.acceleration.add(separation);
+    }
+
     align(boids) {
       let perceptionRadius = 25;
       let steering = createVector();
@@ -94,55 +199,5 @@ class Boid {
       }
       return steering;
     }
-  
-    flock(boids) 
-    {
-        let alignment = this.align(boids);
-        let cohesion = this.cohesion(boids);
-        let separation = this.separation(boids);
-    
-        alignment.mult(alignSlider.value());
-        cohesion.mult(cohesionSlider.value());
-        separation.mult(separationSlider.value());
-    
-        this.acceleration.add(alignment);
-        this.acceleration.add(cohesion);
-        this.acceleration.add(separation);
-    }
-  
-    update() 
-    {
-        this.position.add(this.velocity);
-        this.velocity.add(this.acceleration);
-        this.velocity.limit(this.maxSpeed);
-        this.acceleration.mult(0);
-    
-        // Update frame for animation
-        this.frameCounter++;
-        if (this.frameCounter >= this.frameDelay) 
-        {
-          this.currentFrame = (this.currentFrame + 1) % this.frames.length;
-          this.frameCounter = 0;
-        }
-    }
-  
-    show() 
-    {
-        push();
-        translate(this.position.x, this.position.y);
-        rotate(this.velocity.heading());
-        
-        if (this.frames.length > 0) 
-        {
-          image(
-            this.frames[this.currentFrame],
-            -this.spriteWidth / 2,
-            -this.spriteHeight / 2,
-            this.spriteWidth,
-            this.spriteHeight
-          );
-        }
-        pop();
-    }
-  }
-  
+}
+
--- sketch.js
+++ sketch.js
@@ -3,9 +3,20 @@ let alignSlider, cohesionSlider, separationSlider;
 
 // Preload animation frames
 function preload() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 50; i++) {
     let boid = new Boid();
-    boid.loadFrames('TenderBud/walk_S', 4); // Adjust path and frame count
+
+    // Load frames for cardinal directions
+        boid.loadFrames('north', 'TenderBud/walk_N', 4);
+        boid.loadFrames('south', 'TenderBud/walk_S', 4);
+        boid.loadFrames('east', 'TenderBud/walk_E', 4);
+        boid.loadFrames('west', 'TenderBud/walk_W', 4);
+
+        // Load frames for intercardinal directions
+        boid.loadFrames('northeast', 'TenderBud/walk_NE', 4);
+        boid.loadFrames('southeast', 'TenderBud/walk_SE', 4);
+        boid.loadFrames('southwest', 'TenderBud/walk_SW', 4);
+        boid.loadFrames('northwest', 'TenderBud/walk_NW', 4);
     flock.push(boid);
   }
 }
